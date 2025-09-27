5e7aa00c51aa0785f0902f82de659ceac1d2ffb8
DimaChekashov <foxdima99@gmail.com>
2025-09-27 18:32:57 +0300

    Added a client navigation indicator

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Roboto } from "next/font/google";
 import type { Metadata } from "next";
 import Header from "@/widgets/header";
 import Footer from "@/widgets/footer";
+import NextTopLoader from "nextjs-toploader";
+
 import "@/app/styles/globals.css";
 
 const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
@@ -21,6 +23,7 @@ export default function RootLayout({
       <body
         className={`${roboto.variable} grid grid-rows-[auto_1fr_auto] min-h-screen`}
       >
+        <NextTopLoader />
         <Header />
         <main className="container mx-auto px-6">{children}</main>
         <Footer />
