1c154010c1a368d8a40c8d9d53f85a17c580d375
DimaChekashov <foxdima99@gmail.com>
2025-10-05 00:32:22 +0300

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -5,18 +5,25 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   const sessionId = request.cookies.get("sessionId");
 
-  console.log(sessionId);
+  console.log("sessionId", sessionId);
   console.log(12312312);
 
-  if (sessionId && request.nextUrl.pathname.startsWith("/login")) {
-    return NextResponse.redirect(ROUTES.home);
+  if (sessionId !== undefined) {
+    const { pathname } = request.nextUrl;
+
+    if (
+      pathname.startsWith(ROUTES.signUp) ||
+      pathname.startsWith(ROUTES.login)
+    ) {
+      return NextResponse.redirect(new URL(ROUTES.home, request.url));
+    }
   }
 
   if (
     request.nextUrl.pathname.startsWith("/403") &&
     !request.headers.get("x-middleware-auth")
   ) {
-    return NextResponse.json({ message: "not-found" }, { status: 404 });
+    return NextResponse.redirect(new URL("/not-found", request.url));
   }
 }
 
